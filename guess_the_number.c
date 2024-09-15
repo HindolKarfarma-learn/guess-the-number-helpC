0db5d12562c7aad4e96192e016c70be75ede292c
HindolKarfarma-learn <100765421+HindolKarfarma-learn@users.noreply.github.com>
2024-09-15 04:19:17 -0400

    Added Random number generetor
    use the veriable random as out target number to guess

--- guess_the_number.c
+++ guess_the_number.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-int main(){
+#include <stdlib.h>
+#include <time.h>
 
+int rng(){
+  int n =rand() % 100;
+  return n+1;
+int main(){
+  //using time as seed to make every no. different in every run   . Random will be store the target number to guess 
+  srand(time(0));
+  int Random = rng();
 
 
   return 0;
