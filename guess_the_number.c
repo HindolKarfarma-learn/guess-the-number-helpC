3b6a29063c858b008daa26ab13cacb50ea949a24
Hindol Karfarma <hindolkarfarma@gmail.com>
2024-10-03 15:46:55 +0530

    Simplify Guess funftion to only take the input  and return & Call Guess func from main.

--- guess_the_number.c
+++ guess_the_number.c
@@ -6,19 +6,17 @@ int rng(){
   int n =rand() % 100;
   return n+1;
 }
-int Guess(int Random){
-  int myGuess =0;
-  while (myGuess != Random){
-    printf("guess the number between 1 and 100 :");
-    scanf ("%d",&myGuess);
-  }
+int Guess(){
+  int myGuess;
+  printf("please choose a number between 1 and 100");
+  scanf("%d ",&myGuess);
+  return myGuess;
 }
 int main(){
   //using time as seed to make every no. different in every run   . Random will be store the target number to guess 
   srand(time(0));
   int Random = rng();
-    
-
+  Guess();
   
 
 
